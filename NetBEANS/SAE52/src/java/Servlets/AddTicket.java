e632903a55354d177aff97e9812b9c56bb420ade
Ishac HAMDANI <valetmaxx@gmail.com>
2024-11-10 14:40:37 +0100

    addticket

--- NetBEANS/SAE52/src/java/Servlets/AddTicket.java
+++ NetBEANS/SAE52/src/java/Servlets/AddTicket.java
@@ -1,36 +1,37 @@
 package Servlets;
 
 import DAO.DAOSAE52;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-
 
 /**
- * Servlet ajout ticket
+ * Servlet ajout d'un ticket
  * 
  * @author HAMDANI Ishac
  */
 @WebServlet(name = "AddTicket", urlPatterns = {"/AddTicket"})
 public class AddTicket extends HttpServlet {
-    
+
     //classe permettant de stocker le contenu du JSON de la requête
     private class ticket{
         private String description;
         private String service;
         private String status;
+        private String token;
         private String Test;
         
-        private ticket(String description, String service, String status, String Test){
+        private ticket(String description, String service, String status, String token, String Test){
             this.description = description;
             this.service = service;
             this.status = status;
+            this.token = token;
             this.Test = Test;
         }
     }
@@ -41,13 +42,21 @@ public class AddTicket extends HttpServlet {
      * Ajoute un utilisateur<br><br>
      *
      * Variables à envoyer au servlet (POST)<br>
-     * String prenom       &emsp;&emsp;        prénom de l'utilisateur <br>
-     * String nom       &emsp;&emsp;       nom de l'utilisateur <br>
-     * String login       &emsp;&emsp;         login de l'utilisateur <br>
-     * String password       &emsp;&emsp;      MDP de l'utilisateur (clair) <br>
-     * String role       &emsp;&emsp;      droits de l'utilisateur <br>
+     * 
+     * String processor       &emsp;&emsp;        processeur de l'appareil <br>
+     * String RAM       &emsp;&emsp;        Quantité de RAM (Go) <br>
+     * String macAddress       &emsp;&emsp;        @MAC de la carte réseau <br>
+     * String VLAN       &emsp;&emsp;        VLAN auquel il a accès <br>
+     * String name       &emsp;&emsp;        nom de l'appareil <br>
+     * String serialNumber       &emsp;&emsp;        Numéro de série de l'appareil <br>
+     * String status       &emsp;&emsp;        Status de la machine (maintenance...) <br>
+     * String other       &emsp;&emsp;        inutilisé (clé USB ?) <br>
      * String token       &emsp;&emsp;     token de l'utilisateur connecté <br>
-     * String Test       &emsp;&emsp;      BD à utiliser (true : test | false : sae_52) <br>
+     * String Test       &emsp;&emsp;        BD à utiliser (true : test | false : sae_52) <br><br>
+     * 
+     * Renvoi : <br>
+     * &emsp;   - "Fait" si OK
+     * &emsp;   - "Name exist" si le nom exite déjà dans la BD
      * 
      * @param request       servlet request
      * @param response      servlet response
@@ -72,25 +81,44 @@ public class AddTicket extends HttpServlet {
         String description = ticket.description;
         String service = ticket.service;
         String status = ticket.status;
+        String token = ticket.token;
         Boolean TestBoolean = Boolean.valueOf(ticket.Test);
         
-
-String jsonString;
-
-    try {
-        // Ajouter le ticket dans la base de données
-        DAO.addTicket(description, service, status, TestBoolean);
-        jsonString = "{\"result\":\"Ticket ajouté avec succès\"}";
-    } catch (Exception e) {
-        e.printStackTrace();
-        jsonString = "{\"result\":\"Erreur lors de l'ajout du ticket\"}";
+        
+        //Création du JSON à renvoyer (vide)
+        String jsonString = "";
+        
+        try { 
+            //VERIF si nom en doublon
+            Boolean nameExist = DAO.doNameExist(description, TestBoolean);
+            
+            if(nameExist == false){
+                //verif droits utilisateur demande
+                String userRights = DAO.getUserRightsFromToken(token, TestBoolean);
+                
+                //Verification si l'utilisateur a les droits Admin
+                if(userRights.equals("Admin")){
+                    DAO.addTicket(description, service, status, nameExist);
+                }
+                
+                //JSON renvoyé
+                jsonString = "{\"result\":\"Fait\"}";
+            }
+            else{
+                //JSON renvoyé
+                jsonString = "{\"result\":\"Name exist\"}";
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        //Envoi des données
+        try (PrintWriter out = response.getWriter()) {
+            out.print(jsonString);
+            out.flush();
+        }
     }
-
-    try (PrintWriter out = response.getWriter()) {
-        out.print(jsonString);
-        out.flush();
-    }
-}        
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
