a059de832d8154fff3e9934934f3a6b9ad6454f8
Maxime <134074578+Max51v2@users.noreply.github.com>
2024-12-15 23:49:02 +0100

    Update StartTomcat.sh

--- Serveur/Docker/Config/StartTomcat.sh
+++ Serveur/Docker/Config/StartTomcat.sh
@@ -9,4 +9,7 @@ sleep 5
 curl -u admin:leffe -T /conf/SAE52.war "http://localhost:8080/manager/text/deploy?path=/SAE52&update=true"
 
 #Boucle infinie pour garder le conteneur actif
-tail -f /opt/tomcat/logs/catalina.out
+while true
+do
+sleep 5
+done
